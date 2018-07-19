71461efb1063940e97b8260ec06f74b3b16f1448
Fabio Almeida <fabiorecife@gmail.com>
2018-07-19 19:58:26 -0300

    initial version

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="container">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style src="./assets/sass/app.scss" lang="sass">
+
+</style>
